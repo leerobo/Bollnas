cc3c84f154b4a7609e9c5642090f5f21a459735b
leerobo <lee@sssshhh.com>
2025-04-27 09:42:48 +0200

    err3

--- Bollnas/Common/Managers/hubScanner.py
+++ Bollnas/Common/Managers/hubScanner.py
@@ -10,7 +10,7 @@ from collections import namedtuple
 
 def scan_lan() -> Hubs:
     Attached_list=[]
-    rprint("[orange3]CNTL:     [/orange3][yellow]Scanning ..... [/yellow]" ) 
+    rprint("[orange3]CNTL:     [/orange3][yellow]Scanning ..... [/yellow] {}".format( getConfig().netgear_password ) ) 
     # Fixed static LAN addresses
     rprint("[orange3]CNTL:  static {} : {}".format(len(getConfig().static_sensorhubs),getConfig().static_sensorhubs[0]) ) 
     if len(getConfig().static_sensorhubs) and getConfig().static_sensorhubs[0] != "" :
