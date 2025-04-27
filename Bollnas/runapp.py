ef10a88c053e3e8d9727e9165648f62a9ef0fb41
leerobo <lee@sssshhh.com>
2025-04-27 09:34:16 +0200

    err

--- Bollnas/runapp.py
+++ Bollnas/runapp.py
@@ -20,7 +20,7 @@ import Controller.Blueprint as ControllerRouter
 
 import Common.Managers.pollSensors as pollSensors
 
-from   Common.Config import getConfig, has_config
+from   Common.Config import getConfig
 import Common.Schemas.Sensors.wire1 as wire1
 import Common.Schemas.Sensors.gpio as gpio
 import Common.Models.enums as enums
